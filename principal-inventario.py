13e559e2cc49a82619694434d70b29e5914d5438
GiovannyQuiroz <giovanny.quiroz94@gmail.com>
2022-12-08 18:14:45 -0500

    Solucion de como conocer dias en mora

--- principal-inventario.py
+++ principal-inventario.py
@@ -1,2 +1,6 @@
-from datetime import date, time, datetime
-print(datetime.now())  
+from datetime import date,time,datetime
+localTime = date.today()
+print(localTime)
+movieDate = date(2021, 8, 22)
+operation = movieDate - localTime
+print(operation)
