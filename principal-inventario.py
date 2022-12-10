fef1daa3ff8b50de13a46d8571bc618bb7e8ce33
GiovannyQuiroz <giovanny.quiroz94@gmail.com>
2022-12-09 21:37:35 -0500

    objetos creados

--- principal-inventario.py
+++ principal-inventario.py
@@ -1,3 +1,4 @@
+'''
 from datetime import date,time,datetime
 localTime = date.today()
 print(localTime)
@@ -7,3 +8,21 @@ def fecha(a,m,d):
     operation =  localTime - movieDate
     return operation
 print(fecha(2021,8,22))
+'''
+class persona:
+    nombre: input("Ingrese nombre:\n")
+    apellido: input("Ingrese apellido\n")
+    documento: int(input("Ingrese cedula:\n"))
+    correo: input("Ingrese nombre:\n")
+    clienteId: int(input("Ingrese el id unico\n"))
+
+class pelicula:
+    nombre: input("Ingrese nombre de pelicula:\n")
+    genero: input("Genero al que pertence la pelicula:\n")
+    ano: int(input("Año de publicación:\n"))
+    duracion: input("Duración de la pelicula\n")
+    
+    
+    
+    
+    
