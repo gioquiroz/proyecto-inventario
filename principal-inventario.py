45e0780029fb6fb867deaedaf19d0a5b7e1fb972
GiovannyQuiroz <giovanny.quiroz94@gmail.com>
2022-12-09 20:39:56 -0500

    fecha en funciones para usar input

--- principal-inventario.py
+++ principal-inventario.py
@@ -1,6 +1,9 @@
 from datetime import date,time,datetime
 localTime = date.today()
 print(localTime)
-movieDate = date(2021, 8, 22)
-operation = movieDate - localTime
-print(operation)
+def fecha(a,m,d):
+    print("AA/MM/DD")
+    movieDate = date(a,m,d)
+    operation =  localTime - movieDate
+    return operation
+print(fecha(2021,8,22))
